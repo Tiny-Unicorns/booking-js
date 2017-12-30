411eb9a2f7e770aa34aeb47ad7286d03319fec6e
Kevin Deems <kevindwebsites@gmail.com>
2017-12-30 11:29:01 -0500

    Changes to the survey styling

--- src/main.js
+++ src/main.js
@@ -541,6 +541,7 @@ function TimekitBooking() {
         });
 
     } else {
+        window.isBefore15 = Date.today().isBefore(Date.today().set({ day: 15}));
         var fieldsObject = {
             chosenDate:               moment(eventData.start).format(dateFormat),
             chosenTime:               moment(eventData.start).format(timeFormat) + ' - ' + moment(eventData.end).format(timeFormat),
@@ -554,9 +555,9 @@ function TimekitBooking() {
             successMessageTitle:      config.localization.strings.successMessageTitle,
             successMessageBody:       interpolate.sprintf(config.localization.strings.successMessageBody, '<span class="booked-email"></span>'),
             fields:                   config.bookingFields,
-            dateHere:                 Date.today().isBefore(Date.today().set({ day: 15})),
+            dateHere:                 Date.today().isBefore(Date.today().set({ day: 15})) ? Date.today().set({ day: 15}).toString('MMM dS') : undefined,
             dateHere2:                Date.today().addMonths(1).set({day:1}).toString('MMM dS'),
-            moneyQuestion:            "Do you make over " + (accounting.formatMoney(unitPrice / .3)) + " in Gross Household Monthly Income?",
+            moneyQuestion:            (accounting.formatMoney(unitPrice / .3))
         }
 
         if(!window.iFrameQ){
@@ -572,6 +573,9 @@ function TimekitBooking() {
                     console.log("Survey Done.");
                     //validate that dropdown was completed
                     //send data to endpoint
+                    if(!isBefore15){
+                        // dropdown value is there, is it clicked?
+                    }
                     window.surveyComplete = true;
                     $(".surveyPage").remove();
                 });
